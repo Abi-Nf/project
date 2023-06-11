0091766c99711525e39226f80993ebd8ab83a034
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-06-11 14:50:13 +0300

    feat : added frontend folder

--- server/main.js
+++ server/main.js
@@ -1,13 +1,17 @@
+const socket = require("socket.io")
+const express = require("express");
+const app = express();
 const db = require('./query.js');
 
-const socket = require("socket.io")
+const url_front = "";
+const server = app.listen(4000,function (){});
 
-const io = socket({
-    origin : "url",
+const io = socket(server,{
     cors : {
-        credential : true,
+        origin : url_front,
+        credentials : true
     }
-})
+});
 
 io.on("connect",function(socket){
     socket.on("send-message",function(){})
