198eef795b0ebd56bbbe5c1fcd46f63448556159
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-06-25 18:24:46 +0300

    fix : cors added

--- server/main.js
+++ server/main.js
@@ -1,22 +1,19 @@
 const socket = require("socket.io")
+const cors = require("cors");
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
 const db = require('./query.js');
 const port = 4000;
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
 
 const url_front = "";
 const server = app.listen(port,()=>{
     console.log(`Listen on the port ${port}`);
 });
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 const io = socket(server,{
     cors : {
         origin : url_front,
@@ -25,8 +22,9 @@ const io = socket(server,{
 });
 
 io.on("connect",function(socket){
-    socket.on("send-message",function(){})
-    socket.emit("receive-message","coucou")
+    socket.on("send-message",function({from,to,content_text}){
+        socket.emit("receive-message",content_text);
+    });
 
     socket.emit("receive-notification","");
 })
