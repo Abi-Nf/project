e34ba2126ffcb6b116668d6d104d63fec5fc3554
tendryAxel <hei.tendry@gmail.com>
2023-06-25 16:13:48 +0300

    [Add]:cors configuration

--- server/main.js
+++ server/main.js
@@ -13,6 +13,13 @@ const server = app.listen(port,()=>{
 });
 
 app.use(express.json())
+/**app.use(cors()) *//**a configurer */
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Vous pouvez remplacer "*" par un domaine spécifique pour limiter l'accès
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // Méthodes HTTP autorisées
+  res.header("Access-Control-Allow-Headers", "Content-Type"); // Headers autorisés
+  next();
+});
 
 const io = socket(server,{
     cors : {
