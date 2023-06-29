e658f969334183da19ef0eb1d3a877dd47968f5b
tendryAxel <hei.tendry@gmail.com>
2023-06-29 09:49:21 +0300

    [Fix]:better to read the server code source

--- server/client.js
+++ server/client.js
@@ -21,6 +21,7 @@
 fetch('http://localhost:4000/login/1')
 .then(v=> v.json())
 .then(v=>{
+    console.log(v);
     for (const colomn in v[0])
         document.getElementById("toFill").innerHTML += `<h2>${colomn} : </h2><p>${v[0][colomn]}</p>`;
 })
@@ -32,9 +33,19 @@ fetch('http://localhost:4000/login/1')
 fetch(
     'http://localhost:4000/login',
     {
+        method : 'POST',
         body : JSON.stringify({
             username : "",
             password : "password"
-        })
+        }),
+        headers : {
+            'Content-type' : 'application/json; charset=UTF-8',
+        },
     }
 )
+.then(v=>v.json())
+.then(v=>console.log(v))
+.catch(err=>{
+    if (err)
+        throw err
+})
--- server/query.js
+++ server/query.js
@@ -106,61 +106,109 @@ const logNewUser = ({response}) => {
 const newImg = () => {
     console.log("Add a new image");
 };
-const loginCorrect = ({response}) => {
+const loginTest = ({response}) => {
+    const loginResult = result.rows
+    console.log("Test login");
+    console.log(loginResult);
     response.json({login : true})
 };
-const loginIncorrect = ({response}) => {
-    response.json({login : false})
-};
+
+
 
 function getOneUser(request, response){
     const uuid = parseInt(request.params.uuid);
-    pool.query(
-        allPsql["oneUser"]({uuid}),
-        (err, result)=>errAndResult(
-            err,
-            result,
-            sendRow,
-            {response}
-        )
+    pool.query(`
+        SELECT "user".id,first_name,filename FROM "user"
+        FULL OUTER JOIN image
+        ON "user".id = image.id_user
+        WHERE "user".id=${uuid}; 
+    `,
+        (err, result)=>{
+            if (err)
+                throw err
+            response.status(200).json(result.rows);
+        }
     )
 };
 
 function addImage(request, response){
-    pool.query(
-        allPsql["validSignUp"](request.file),
-        (err, result)=>errAndResult(
-            err, result,
-            newImg
-        )
+    const {originalname, encoding ,mimetype, filename, path, size, destination} = request.body;
+    pool.query(`
+        INSERT INTO image(
+            originalname,
+            encoding,
+            mimetype,
+            filename,
+            path,
+            size,
+            destination
+        ) VALUES (
+            '${originalname}',
+            '${encoding}',
+            '${mimetype}',
+            '${filename}',
+            '${path}',
+            '${String(size)}',
+            '${destination}'
+        );
+    `,
+            (err, result)=>{
+                if (err)
+                    throw err
+                console.log("Add a new image");
+            }
     )
     response.status(200).json({send : true});
 };
 
 function createAccount(request, response){
-    pool.query(
-        allPsql["addUser"](request.body),
-        (err, result)=>errAndResult(
-            err,
-            result,
-            logNewUser,
-            {response}
+    const {firstname,lastname,birthdate,gender,email, phone,password} = request.body;
+    pool.query(`
+        INSERT INTO "user"(
+            first_name,
+            last_name,
+            pseudo,
+            birth_date,
+            gender,
+            email,
+            phone,
+            password
+        ) VALUES (
+            '${firstname}',
+            '${lastname}',
+            '${pseudo}'
+            '${birthdate}',
+            '${gender}',
+            '${email}',
+            '${phone}',
+            '${password}'
         )
+        RETURNING id;
+    `,
+    (err, result)=>{
+        if (err)
+            throw err
+        response.status(200).json(result);
+    }
     )
 };
 
 function login(request, response){
-    try{
-        pool.query(
-            allPsql['validLogin'](request.body),
-            (err, result)=>errAndResult(
-                err, result,
-                loginCorrect, {response}
-            )
-        )
-    } catch (err) {
-        loginIncorrect({response})
-    };
+    const {username,password} = request.body;
+    pool.query(()=>`
+        SELECT id FROM "user"
+        WHERE pseudo = '${username}'
+        AND password = '${password}';
+    `,
+        (err, result)=>{
+            if (err)
+                throw err
+            const loginResult = result.rows
+            console.log("Test login");
+            console.log(loginResult);
+            response.json({login : true})
+        }
+    )
 };
 
 module.exports = {
