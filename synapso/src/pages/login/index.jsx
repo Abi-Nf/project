5a2fe65e65fe10f10a504b52f36acda4a893e3f2
Fiantso Harena <fiantsorav@gmail.com>
2023-07-04 07:42:19 +0300

    Fix: Fixing and finalizing the main form of the login page

--- synapso/src/pages/login/index.jsx
+++ synapso/src/pages/login/index.jsx
@@ -7,16 +7,16 @@ import signInImage from "../../assets/illustrations/World-cuate.svg";
 import "./index.css";
 
 const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
-  const container = document.querySelector(".container");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-  sign_up_btn.addEventListener('click', () => {
-    container.classList.add("sign-up-mode");
-  });
+sign_up_btn.addEventListener('click', () => {
+	container.classList.add("sign-up-mode");
+});
 
-  sign_in_btn.addEventListener('click', () => {
-    container.classList.remove("sign-up-mode");
-  });
+sign_in_btn.addEventListener('click', () => {
+	container.classList.remove("sign-up-mode");
+});
 
 const Login = () => {
 	return (
